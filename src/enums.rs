4c25ddb4dd391af05a20d38df2dbc7245f319c9e
elcharitas <jonathanirhodia@gmail.com>
2023-02-10 22:59:26 +0100

    feat: create program enums

---
+++ src/enums.rs
@@ -0,0 +1,34 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Char(char),
+    Number(f64),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    LParen,
+    RParen,
+    Identifier(String),
+    Equal,
+    Semicolon,
+    Print,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expression {
+    Number(f64),
+    Plus(Box<Expression>, Box<Expression>),
+    Minus(Box<Expression>, Box<Expression>),
+    Asterisk(Box<Expression>, Box<Expression>),
+    Slash(Box<Expression>, Box<Expression>),
+    Variable(String),
+    Decimal(Box<Expression>, Box<Expression>),
+    Power(Box<Expression>, Box<Expression>),
+    None,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Statement {
+    Expression(Expression),
+    Assignment(String, Expression),
+}
